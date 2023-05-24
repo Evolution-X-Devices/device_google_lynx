57cf756f56f0070516154a7e7dd30920a63304ff
AnierinB <anierin@evolution-x.org>
2023-05-24 16:42:19 -0700

    lynx: Ship PixelParts
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device-evolution.mk
+++ device-evolution.mk
@@ -21,6 +21,9 @@ PRODUCT_PACKAGES += \
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
 # Build necessary packages for vendor
 
 # Audio
